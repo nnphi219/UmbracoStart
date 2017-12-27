de7ab7053e1412c4b4f9705f90c12bd4cd95e0c6
ORIENTSOFTWARE\nnphi <nnphi219@gmail.com>
2017-12-27 16:22:43 +0700

    Create Master Page

---
+++ UmbracoStart/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Web.Mvc;
+using System.Web.Mvc;
+
+namespace UmbracoStart.Controllers
+{
+    public class HomeController : SurfaceController
+    {
+        private const string PARTIAL_VIEW_FOLDER = "~/Views/Partials/Home/";
+
+        public ActionResult RenderFeatured()
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Featured.cshtml");
+        }
+
+        public ActionResult RenderServices()
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Services.cshtml");
+        }
+
+        public ActionResult RenderBlog()
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Blog.cshtml");
+        }
+
+        public ActionResult RenderClients()
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Clients.cshtml");
+        }
+    }
+}
